19fe0799af49b0e6cb65655dc8f909bf96cfdb79
ShinJaeMin98 <jaemin6292@gmail.com>
2023-12-30 02:34:54 +0900

    초기 업로드

---
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.choongang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
