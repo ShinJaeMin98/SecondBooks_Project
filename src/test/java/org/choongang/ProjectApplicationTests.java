8de982240a74b9528f6a80d5e973dc162cc9880c
ShinJaeMin98 <jaemin6292@gmail.com>
2023-12-30 03:43:13 +0900

    의존성 및 설정파일 추가

--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -2,8 +2,10 @@ package org.choongang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
 class ProjectApplicationTests {
 
 	@Test
