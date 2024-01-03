b7aba590be64e753c9a0a22a4250b6864cface44
ShinJaeMin98 <jaemin6292@gmail.com>
2024-01-04 02:33:10 +0900

    관리자 구성

---
+++ src/main/java/org/choongang/admin/member/controllers/MemberController.java
@@ -0,0 +1,35 @@
+package org.choongang.admin.member.controllers;
+
+import org.choongang.admin.menus.Menu;
+import org.choongang.admin.menus.MenuDetail;
+import org.choongang.commons.ExceptionProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller("adminMemberController")
+@RequestMapping("/admin/member")
+public class MemberController implements ExceptionProcessor {
+
+    @ModelAttribute("menuCode")
+    public String getMenuCode() {
+        return "member";
+    }
+
+    @ModelAttribute("subMenus")
+    public List<MenuDetail> getSubMenus() {
+
+        return Menu.getMenus("member");
+    }
+
+    @GetMapping
+    public String list(Model model) {
+
+        model.addAttribute("subMenuCode", "list");
+        return "admin/member/list";
+    }
+}
