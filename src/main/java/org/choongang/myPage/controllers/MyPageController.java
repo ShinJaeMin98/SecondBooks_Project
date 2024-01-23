3d6f6536a2c863d42a27331eae32372a03b58fe1
ShinJaeMin98 <jaemin6292@gmail.com>
2024-01-23 10:58:33 +0900

    마이페이지 프로필 추가

--- src/main/java/org/choongang/myPage/controllers/MyPageController.java
+++ src/main/java/org/choongang/myPage/controllers/MyPageController.java
@@ -1,21 +1,77 @@
 package org.choongang.myPage.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.board.controllers.BoardDataSearch;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.service.SaveBoardDataService;
 import org.choongang.commons.ExceptionProcessor;
+import org.choongang.commons.ListData;
 import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/myPage")
 public class MyPageController implements ExceptionProcessor {
 
+    private final SaveBoardDataService saveBoardDataService;
+
     public final Utils utils;
 
-    @GetMapping
-    public String home() {
+    @GetMapping // 마이페이지 메인
+    public String index(Model model) {
+        commonProcess("main", model);
+
         return utils.tpl("myPage/myPage");
+    }
+
+    /**
+     * 찜 게시글 목록
+     *
+     * @param search
+     * @param model
+     * @return
+     */
+    @GetMapping("/save_post")
+    public String savePost(@ModelAttribute BoardDataSearch search, Model model) {
+        commonProcess("save_post", model);
+
+        ListData<BoardData> data = saveBoardDataService.getList(search);
+
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
+
+        return utils.tpl("mypage/save_post");
+    }
+
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "main";
+        String pageTitle = Utils.getMessage("마이페이지", "commons");
+
+        List<String> addCss = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        List<String> addCommonScript = new ArrayList<>();
+
+
+        if (mode.equals("save_post")) { // 찜한 게시글 페이지
+            pageTitle = Utils.getMessage("찜_게시글", "commons");
+
+            addScript.add("board/common");
+            addScript.add("myPage/save_post");
+        }
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("addCss", addCss);
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("addCommonScript", addCommonScript);
     }
 }
