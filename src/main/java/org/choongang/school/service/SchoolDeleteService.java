08bda9c12ece089dc35ab90380c7ee1cfb4051b3
Joonbaamm <junbum316@naver.com>
2024-01-24 17:08:44 +0900

    학교삭제시 사진도 디비에서 삭제

--- src/main/java/org/choongang/school/service/SchoolDeleteService.java
+++ src/main/java/org/choongang/school/service/SchoolDeleteService.java
@@ -1,6 +1,7 @@
 package org.choongang.school.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.file.service.FileDeleteService;
 import org.choongang.school.repositories.SchoolRepository;
 import org.choongang.school.entities.School;
 import org.springframework.stereotype.Service;
@@ -13,16 +14,24 @@ public class SchoolDeleteService {
 
     private final SchoolRepository repository;
 
+    private final FileDeleteService fileDeleteService;
+
     public void delete (Long num){
         School school = repository.findById(num).orElse(null);
 
+        String gid = school.getGid();
+
         repository.delete(school);
+
+        fileDeleteService.delete(gid);
+
+        repository.flush();
 
     }
     public void deleteChks (List<Long> chks){
         for(Long num : chks){
             School school = repository.findById(num).orElse(null);
-            repository.delete(school);
+            delete(school.getNum());
             repository.flush();
         }
     }
