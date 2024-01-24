1c0ece9648a7e3ea793cf6a5a01dd94fcd56d165
cbmja <cbmja@naver.com>
2024-01-24 14:58:04 +0900

    .

--- src/test/java/org/choongang/school/SchoolUtilTest.java
+++ src/test/java/org/choongang/school/SchoolUtilTest.java
@@ -1,5 +1,6 @@
 package org.choongang.school;
 
+import org.choongang.commons.ExcelUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SchoolUtilTest {
     @Autowired
     private  SchoolUtil util;
+    @Autowired
+    private ExcelUtils excelUtils;
 
     @Test
     void test1() {
@@ -25,4 +28,36 @@ public class SchoolUtilTest {
         assertEquals("카카오", schoolName);
         assertEquals("kakao.com", domain);
     }
-}
+
+    @Test
+    void test2() {
+
+            List<String[]> list = excelUtils.getData("data/schools.xlsx", new int[] {0, 1}, 0);
+            String schools = "";
+            int k = 0;
+            for(String[] l : list){
+                //l[0] : 학교명 , l[1] : 도메인
+                int i = l[0].indexOf(" ");
+                if(i != -1){
+                    l[0] = l[0].substring(0,i).trim();
+                }
+                l[1] = l[1].replace("www.","");
+                l[1] = l[1].replaceAll("/" , "");
+
+                int v = schools.indexOf(l[0]);
+
+                if(!l[1].equals("") || l[1] != null){
+                    if(v == -1){
+                        schools += k+l[0]+"_"+l[1]+"\n";
+                    }
+                }
+                k++;
+                if(k == 225){
+                    break;
+                }
+            }
+            System.out.println(schools);
+
+        }
+    }
+
